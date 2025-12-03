54b0ff529cd03d4a2ca3baf25ba78d20dbc6c4f9
Jonathan Pearlin <jpearlin1@gmail.com>
2025-12-03 14:43:04 -0500

    Formatting

--- day3.go
+++ day3.go
@@ -11,23 +11,23 @@ func day3Part1() {
 	if err != nil {
 		fmt.Printf("Error reading from input file: %v\n", err)
 	} else {
-	    total := 0
+		total := 0
 		for _, line := range lines {
 			joltage := 0
-		    for i := 0; i < len(line); i++ {
-		        digit := line[i]
-		        rest := line[(i+1):]
+			for i := 0; i < len(line); i++ {
+				digit := line[i]
+				rest := line[(i + 1):]
 				for _, char := range rest {
-				    value, _ := strconv.Atoi(string(digit) + string(char))
-				    if value > joltage {
-				        joltage = value
-				    }
+					value, _ := strconv.Atoi(string(digit) + string(char))
+					if value > joltage {
+						joltage = value
+					}
 				}
-		    }
-            fmt.Printf("In %s, you can make the largest joltage possible %d.\n", line, joltage)
-            total += joltage
+			}
+			fmt.Printf("In %s, you can make the largest joltage possible %d.\n", line, joltage)
+			total += joltage
 		}
-        fmt.Printf("The total output joltage is %d.\n", total)
+		fmt.Printf("The total output joltage is %d.\n", total)
 	}
 }
 
@@ -36,41 +36,41 @@ func day3Part2() {
 	if err != nil {
 		fmt.Printf("Error reading from input file: %v\n", err)
 	} else {
- 	    limit := 12
-	    total := 0
-        for _, line := range lines {
-            var joltage [12]int
-            batteryIndex := 0
-            for i := 0; i < len(line); i++ {
-                digit, _ := strconv.Atoi(string(line[i]))
-                if joltage[batteryIndex] <= digit {
-                    if len(line) - i < (limit - batteryIndex) {
-                        batteryIndex = min((limit - 1), batteryIndex + 1)
-                        joltage[batteryIndex] = digit
-                    } else {
-                        joltage[batteryIndex] = digit
-                    }
-                } else if batteryIndex == limit - 1 {
-                    if joltage[batteryIndex] <= digit {
-                        joltage[batteryIndex] = digit
-                    }
-                } else {
-                    batteryIndex++
-                    joltage[batteryIndex] = digit
-                }
-            }
-            joltageValue := arrayToInt(joltage)
-            fmt.Printf("In %s, you can make the largest joltage possible %d.\n", line, joltageValue)
-            total += joltageValue
+		limit := 12
+		total := 0
+		for _, line := range lines {
+			var joltage [12]int
+			batteryIndex := 0
+			for i := 0; i < len(line); i++ {
+				digit, _ := strconv.Atoi(string(line[i]))
+				if joltage[batteryIndex] <= digit {
+					if len(line)-i < (limit - batteryIndex) {
+						batteryIndex = min((limit - 1), batteryIndex+1)
+						joltage[batteryIndex] = digit
+					} else {
+						joltage[batteryIndex] = digit
+					}
+				} else if batteryIndex == limit-1 {
+					if joltage[batteryIndex] <= digit {
+						joltage[batteryIndex] = digit
+					}
+				} else {
+					batteryIndex++
+					joltage[batteryIndex] = digit
+				}
+			}
+			joltageValue := arrayToInt(joltage)
+			fmt.Printf("In %s, you can make the largest joltage possible %d.\n", line, joltageValue)
+			total += joltageValue
 		}
-        fmt.Printf("The total output joltage is %d.\n", total)
+		fmt.Printf("The total output joltage is %d.\n", total)
 	}
 }
 
 func arrayToInt(a [12]int) int {
-    result := 0
-    for _, digit := range a {
-        result = result*10 + digit
-    }
-    return result
+	result := 0
+	for _, digit := range a {
+		result = result*10 + digit
+	}
+	return result
 }
